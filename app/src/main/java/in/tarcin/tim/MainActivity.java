030a7195699caf8127c435780362949b9b3b1b50
MK <atrvicky@live.com>
2020-01-17 06:27:03 +0530

    First commit. Nothing much

---
+++ app/src/main/java/in/tarcin/tim/MainActivity.java
@@ -0,0 +1,14 @@
+package in.tarcin.tim;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
